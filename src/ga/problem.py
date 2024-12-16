evaluate function bugfix when computing variance

src/ga/problem.py
@@ -1,8 +1,30 @@
 from jmetal.core.problem import IntegerProblem
 import numpy as np
+import numpy.typing as npt
 import random
 from jmetal.core.problem import IntegerSolution
 from ..models import Problem
+
+
+def evaluate(
+    data: Problem, assignments: npt.NDArray
+) -> tuple[np.floating, np.floating]:
+    task_preferences = np.sum(assignments * data.task_preferences, axis=1)
+
+    acc_group_preferences = (data.group_preferences @ assignments) * assignments
+    mean_group_preference_by_task_worker = acc_group_preferences / data.task_demand
+    mean_group_preference_by_worker = mean_group_preference_by_task_worker.sum(axis=1)
+
+    preferences = task_preferences + mean_group_preference_by_worker
+
+    # normalize
+    tasks_by_worker = assignments.sum(axis=1)
+    preference_score = np.sum(preferences / tasks_by_worker)
+
+    f1 = -preference_score
+    f2 = np.var(assignments @ data.task_duration)
+
+    return (f1, f2)
 
 
 class CustomProblem(IntegerProblem):
@@ -35,9 +57,6 @@ class CustomProblem(IntegerProblem):
         return 0
 
     def evaluate(self, solution: IntegerSolution) -> IntegerSolution:
-        # preference_score = 0
-        # work_load = np.zeros(shape=self.data.total_workers)
-
         total_tasks = self.data.total_tasks
         total_workers = self.data.total_workers
 
@@ -46,75 +65,12 @@ class CustomProblem(IntegerProblem):
             (total_workers, total_tasks)
         )
 
-        task_preferences = np.sum(assignments * self.data.task_preferences, axis=1)
+        f1, f2 = evaluate(self.data, assignments)
 
-        acc_group_preferences = (
-            self.data.group_preferences @ assignments
-        ) * assignments
-        mean_group_preference_by_task_worker = (
-            acc_group_preferences / self.data.task_demand
-        )
-        mean_group_preference_by_worker = mean_group_preference_by_task_worker.sum(
-            axis=1
-        )
-
-        preferences = task_preferences + mean_group_preference_by_worker
-
-        # normalize
-        tasks_by_worker = assignments.sum(axis=1)
-        preference_score = np.sum(preferences / tasks_by_worker)
-
-        solution.objectives[0] = -preference_score
-        solution.objectives[1] = np.var(assignments @ self.data.task_demand)
+        solution.objectives[0] = f1
+        solution.objectives[1] = f2
 
         return solution
-        # task_workers = [
-        #     [
-        #         w
-        #         for w in range(total_workers)
-        #         if solution.variables[total_tasks * w + t] == 1
-        #     ]
-        #     for t in range(total_tasks)
-        # ]
-        # acc_preferences = [0.0] * total_workers
-        # assignments_per_worker = [
-        #     [
-        #         t
-        #         for t in range(total_tasks)
-        #         if solution.variables[total_tasks * w + t] == 1
-        #     ]
-        #     for w in range(total_workers)
-        # ]
-        #
-        # for t, workers in enumerate(task_workers):
-        #     # work load
-        #     for w1 in workers:
-        #         work_load[w1] += self.data.task_duration[t]
-        #
-        #         # task preference
-        #         acc_preferences[w1] += self.data.task_preferences[w1][t]
-        #
-        #         # group preference
-        #         acc = 0.0
-        #         for w2 in workers:
-        #             acc += self.data.group_preferences[w1][w2]
-        #         acc_preferences[w1] += acc / len(workers)
-        #
-        # preference_score = (
-        #     sum(
-        #         [
-        #             acc_preferences[w] / len(tasks)
-        #             for w, tasks in enumerate(assignments_per_worker)
-        #             if tasks
-        #         ]
-        #     )
-        #     / total_workers
-        # )
-        #
-        # solution.objectives[0] = -1.0 * preference_score
-        # solution.objectives[1] = float(np.var(work_load))
-        #
-        # return solution
 
     def create_solution(self) -> IntegerSolution:
         new_solution = IntegerSolution(